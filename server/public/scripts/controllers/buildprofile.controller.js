0dce5421621c9387e73ed4a75eabd93b47dce3e9
Tom Brossart <tom@blueraven.digital>
2017-08-09 15:35:16 -0500

    core motivation function clientside almost complete

--- server/public/scripts/controllers/buildprofile.controller.js
+++ server/public/scripts/controllers/buildprofile.controller.js
@@ -1,6 +1,9 @@
 myApp.controller('BuildController', function($scope, $http, $location, UserService, $mdBottomSheet, $mdSidenav,  $mdDialog) {
   console.log('BuildController created');
 
+  $scope.selectMotivation = function(tile) {
+    console.log("user selected", tile);
+  };
 
   this.tiles = buildGridModel({
     icon : "",
@@ -23,33 +26,33 @@ myApp.controller('BuildController', function($scope, $http, $location, UserServi
         it.icon = "attach_money";
         it.title = "Financial";
         it.background = "red";
-        it.span.row = it.span.col = 2;
+        it.span.row = it.span.col = 0.5;
         break;
         case 2:
         it.background = "green";
-        it.icon = "attach_money";
-        it.title = "Financial";
+        it.icon = "favorite";
+        it.title = "Health/Wellness";
         break;
         case 3:
-        it.icon = "attach_money";
-        it.title = "Financial";
+        it.icon = "public";
+        it.title = "Environmental";
         it.background = "darkBlue";
         break;
         case 4:
         it.icon = "attach_money";
-        it.title = "Financial";
+        it.title = "Adventure";
         it.background = "blue";
         it.span.col = 2;
         break;
         case 5:
-        it.icon = "attach_money";
-        it.title = "Financial";
-        it.background = "yellow";
-        it.span.row = it.span.col = 2;
+        it.icon = "people";
+        it.title = "Peer Pressure";
+        it.background = "red";
+        it.span.row = it.span.col = 0.5;
         break;
         case 6:
-        it.icon = "attach_money";
-        it.title = "Financial";
+        it.icon = "gesture";
+        it.title = "Other";
         it.background = "pink";
         break;
         //    case 7: it.background = "darkBlue";      break;
