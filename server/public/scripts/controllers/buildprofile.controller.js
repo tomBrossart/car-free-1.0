72448cfa02ab024e8b168efef07c6e72e83e951f
Tom Brossart <tom@blueraven.digital>
2017-08-14 15:28:10 -0500

    starting project pivot for week 2

--- server/public/scripts/controllers/buildprofile.controller.js
+++ server/public/scripts/controllers/buildprofile.controller.js
@@ -117,7 +117,7 @@ myApp.controller('BuildController', function($scope, $http, $location, UserServi
     return results;
   }
 
-  $scope.needs = ["Work", "Family", "Social", "Adventure", "Shopping"];
-  $scope.trips = [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  $scope.avgs = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 99999999];
+  $scope.trips = [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
 
 });
